1a62268b235a3497efab96a1fec2d2349089655a
Fa-31-an <fsosaolea@gmail.com>
2023-07-13 18:26:06 -0300

    cart reducer + provider + actions

--- src/context/CartProvider.jsx
+++ src/context/CartProvider.jsx
@@ -1,46 +1,25 @@
 import { createContext, useReducer } from 'react';
 import PropTypes from 'prop-types';
-import { TabContent } from 'react-bootstrap';
+import { actionTypes } from '../actions/cart.actions';
+import { cartInitialState, cartReducer } from '../reducers/cart.reducer';
 
 const CartContext = createContext();
-
-const cartInitialState = {
-  itemsInCart: [],
-};
-
-const actionTypes = {
-  ADD_TO_CART: "ADD_TO_CART",
-  REMOVE_ONE_FROM_CART: "REMOVE_ONE_FROM_CART",
-  REMOVE_ALL_FROM_CART: "REOMVE_ALL_FROM_CART",
-  CLEAR_CART: "CLEAR_CART",
-};
-
-function cartReducer(state, action) {
-  const payload = action.payload;
-  switch (action.type) {
-    case actionTypes.ADD_TO_CART:
-      return {
-        ...state,
-        itemsInCart: [...state.itemsInCart, payload],
-      }
-      break;
-  };
-};
 
 function CartProvider({ children }) {
   const [state, dispatch] = useReducer(cartReducer, cartInitialState);
 
   function addToCart(drink) {
-    dispatch({type: actionTypes.ADD_TO_CART, payload: drink});
+    dispatch({ type: actionTypes.ADD_TO_CART, payload: drink });
   };
-
-  function removeOneFromCart() { };
-
-  function removeAllFromCart() { };
-
-  function clearCart() { };
-
-
+  function removeOneFromCart(idDrink) {
+    dispatch({ type: actionTypes.REMOVE_ONE_FROM_CART, payload: { idDrink } })
+  };
+  function removeAllFromCart(idDrink) {
+    dispatch({ type: actionTypes.REMOVE_ALL_FROM_CART, payload: { idDrink } })
+  };
+  function clearCart() {
+    dispatch({ type: actionTypes.CLEAR_CART })
+  };
   const cartValues = {
     cart: state,
     addToCart,
---
+++ src/reducers/cart.reducer.js
@@ -0,0 +1,70 @@
+import { actionTypes } from '../actions/cart.actions';
+
+export const cartInitialState = {
+  itemsInCart: [],
+};
+
+export function cartReducer(state, { type, payload }) {
+  const { idDrink } = payload;
+
+  let drinkInCart = state.itemsInCart.find((item) => item.idDrink === idDrink);
+  switch (type) {
+    case actionTypes.ADD_TO_CART:
+      if (drinkInCart) {
+
+        let cartItemsUpdated = state.itemsInCart.map(item => {
+
+          if (item.idDrink === idDrink) {
+            return {
+              ...item,
+              quantity: item.quantity + 1,
+            };
+          };
+
+          return item;
+        });
+        return {
+          ...state,
+          itemsInCart: cartItemsUpdated,
+        };
+      } else {
+        payload.quantity = 1;
+        return {
+          ...state,
+          itemsInCart: [...state.itemsInCart, payload],
+        };
+      };
+
+    case actionTypes.REMOVE_ONE_FROM_CART:
+      if (drinkInCart > 1) {
+        let cartUpdated = state.itemsInCart.forEach(item => {
+          if (item.idDrink === idDrink) {
+            return {
+              ...item,
+              quantity: item.quantity - 1,
+            };
+          };
+          return item;
+        })
+        return {
+          ...state,
+          itemsInCart: cartUpdated,
+        };
+      } else {
+        let cartUpdated = state.itemsInCart.filter(item => !item.idDrink === idDrink)
+        return {
+          ...state,
+          itemsInCart: cartUpdated,
+        };
+      };
+
+    case actionTypes.REMOVE_ALL_FROM_CART:
+      if (drinkInCart) {
+        let cartUpdated = state.cartItems.filter(item => !item.idRink === idDrink);
+        return { ...state, itemsInCart: cartUpdated };
+      };
+
+    case actionTypes.CLEAR_CART:
+      return { ...state, itemsInCart: [] };
+  };
+};
